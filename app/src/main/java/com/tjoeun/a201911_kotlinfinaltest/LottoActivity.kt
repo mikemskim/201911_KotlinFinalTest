b2e8fb97d8b462bc0106cb90b3fd4b43ea29aed9
mikemskim <mskim@steconsulting.co.kr>
2019-11-25 22:13:27 +0900

    구매 중단/재개 기능 추가

--- app/src/main/java/com/tjoeun/a201911_kotlinfinaltest/LottoActivity.kt
+++ app/src/main/java/com/tjoeun/a201911_kotlinfinaltest/LottoActivity.kt
@@ -10,14 +10,24 @@ class LottoActivity : BaseActivity() {
 
     var mHandler = Handler()
 
+    var isNowBuying = false
 
+    var firstRankCount = 0
+    var secondRankCount = 0
+    var thirdRankCount = 0
+    var fourthRankCount = 0
+    var fifthRankCount = 0
+    var wrongCount = 0
 
 //     누적 사용금액
     var usedMoney = 0L
 //    누적 당첨금액
     var luckyMoney = 0L
 
+//    기존 당첨 번호 저장 변수
     var lottoNumArrayList = ArrayList<Int>()
+    var bonusNum = 0
+
     var thisWeekLottoNumTextViewArrayList = ArrayList<TextView>()
 
     var myNumberArrayList = ArrayList<Int>()
@@ -43,8 +53,16 @@ class LottoActivity : BaseActivity() {
         }
 
         autoLottoBtn.setOnClickListener {
+            if (!isNowBuying) {
+                doLottoLoop()
+                isNowBuying = true
+                autoLottoBtn.text = "구매 중단"
+            } else {
+                stopLottoLoop()
+                isNowBuying = false
+                autoLottoBtn.text = "자동 구매 재개"
+            }
 
-            doLottoLoop()
 //            while (true) {
 //
 //
@@ -82,31 +100,56 @@ class LottoActivity : BaseActivity() {
             6 -> {
 //                Toast.makeText(mContext, "1등 당첨", Toast.LENGTH_SHORT).show()
                 luckyMoney += 2000000000
+                firstRankCount++
             }
             5 -> {
+                var isSecondRank = true
+
+                if (!myNumberArrayList.contains(bonusNum)) {
+                    isSecondRank = false
+                }
+
 //                Toast.makeText(mContext, "3등 당첨", Toast.LENGTH_SHORT).show()
+                if (isSecondRank) {
+                    luckyMoney += 65000000
+                    secondRankCount++
+                } else {
+                    luckyMoney += 1500000
+                    thirdRankCount++
+                }
+
                 luckyMoney += 1500000
+                thirdRankCount++
             }
             4 -> {
 //                Toast.makeText(mContext, "4등 당첨", Toast.LENGTH_SHORT).show()
                 luckyMoney += 50000
+                fourthRankCount++
             }
             3 -> {
 //                Toast.makeText(mContext, "5등 당첨", Toast.LENGTH_SHORT).show()
-                luckyMoney += 5000
+                usedMoney -= 5000
+                fifthRankCount++
             }
             else -> {
 //                Toast.makeText(mContext, "꽝", Toast.LENGTH_SHORT).show()
                 luckyMoney += 0
+                wrongCount++
             }
         }
 
         luckyMoneyTxt.text = String.format("누적 당첨 금액 : %,d원", luckyMoney)
+        firstRankCountTxt.text = String.format("1등 횟수 : %,d", firstRankCount)
+        secondRankCountTxt.text = String.format("2등 횟수 : %,d", secondRankCount)
+        thirdRankCountTxt.text = String.format("3등 횟수 : %,d", thirdRankCount)
+        fourthRankCountTxt.text = String.format("4등 횟수 : %,d", fourthRankCount)
+        fifthRankCountTxt.text = String.format("5등 횟수 : %,d", fifthRankCount)
+        wrongCountTxt.text = String.format("꽝 횟수 : %,d", wrongCount)
     }
 
 
-    fun doLottoLoop() {
-        mHandler.post {
+    var lottoRunnable = object : Runnable {
+        override fun run() {
             if (usedMoney < 100000000) {
                 setThisWeekLottoNum()
                 checkLottoRank()
@@ -119,6 +162,17 @@ class LottoActivity : BaseActivity() {
                 }
             }
         }
+
+    }
+
+    fun doLottoLoop() {
+        // 할일 등록
+        mHandler.post(lottoRunnable)
+    }
+
+    fun stopLottoLoop() {
+        // 할일 제거
+        mHandler.removeCallbacks(lottoRunnable)
     }
 
     fun setThisWeekLottoNum() {
@@ -155,12 +209,34 @@ class LottoActivity : BaseActivity() {
 //        당첨번호 6개를 작은 숫자부터 큰 숫자 순서대로 (정렬)
         lottoNumArrayList.sort()
 
+//        보너스 번호를 추가로 뽑자, 중복을 피해야함. => 몇번이나 뽑아야 중복이 아닐지 알 수 없다.
+
+        while (true) {
+            var tempRandomNum = (Math.random() * 45 + 1).toInt()
+            var isDupNum = true
+
+            for (num in lottoNumArrayList) {
+                if (num == tempRandomNum) {
+                    isDupNum = false
+                    break
+                }
+            }
+
+            if (isDupNum) {
+                bonusNum = tempRandomNum
+                break
+            }
+        }
+
+
         for (i in 0..(lottoNumArrayList.size-1)) {
             var numTxt = thisWeekLottoNumTextViewArrayList.get(i)
             var number = lottoNumArrayList.get(i)
 
             numTxt.text = number.toString()
         }
+
+        bonusNumTxt.text = bonusNum.toString()
     }
 
     override fun setValues() {
