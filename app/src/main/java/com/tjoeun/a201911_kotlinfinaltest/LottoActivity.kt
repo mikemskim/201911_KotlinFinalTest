b039f941936dad3e636648304d0c1a80b905b02d
mikemskim <mskim@steconsulting.co.kr>
2019-11-25 20:55:03 +0900

    내 마킹 숫자 들을 Array List에 담아 두기

--- app/src/main/java/com/tjoeun/a201911_kotlinfinaltest/LottoActivity.kt
+++ app/src/main/java/com/tjoeun/a201911_kotlinfinaltest/LottoActivity.kt
@@ -1,10 +1,16 @@
 package com.tjoeun.a201911_kotlinfinaltest
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_lotto.*
 
 class LottoActivity : BaseActivity() {
 
+    var lottoNumArrayList = ArrayList<Int>()
+    var thisWeekLottoNumTextViewArrayList = ArrayList<TextView>()
+
+    var myNumberArrayList = ArrayList<Int>()
+    var myTextViewArrayList = ArrayList<TextView>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,9 +21,86 @@ class LottoActivity : BaseActivity() {
 
     override fun setupEvents() {
 
+        buyOneLottoBtn.setOnClickListener {
+//          숫자를 랜덤으로 6개 생성
+            setThisWeekLottoNum()
+
+            checkLottoRank()
+        }
+    }
+
+//    당첨 결과를 체크 몇등인지 확인
+    fun checkLottoRank() {
+
+//    6개 : 1등 => 20억원
+//    5개 : 3등 => 150 만원
+//    4개 : 4등 => 5만원
+//    3개 : 5등 => 5천원
+//    그 이하 : 꽝 => 0 원
+
+
+    }
+
+    fun setThisWeekLottoNum() {
+
+//        당첨번호는 모두 날리고 다시 뽑자
+        lottoNumArrayList.clear()
+
+        for (lottoNumTxt in thisWeekLottoNumTextViewArrayList) {
+
+            var randomNum = 0
+            while (true) {
+                randomNum = (Math.random() * 45 +1).toInt()
+
+                var isDupOK = true
+                for (num in lottoNumArrayList) {
+                    if(num == randomNum) {
+//                        중복되는 숫자를 발견
+                        isDupOK = false
+                        break
+                    }
+                }
+
+                if (isDupOK) {
+
+                    lottoNumArrayList.add(randomNum)
+                    break
+                }
+            }
+
+
+//            lottoNumTxt.text = randomNum.toString()
+        }
+
+//        당첨번호 6개를 작은 숫자부터 큰 숫자 순서대로 (정렬)
+        lottoNumArrayList.sort()
+
+        for (i in 0..(lottoNumArrayList.size-1)) {
+            var numTxt = thisWeekLottoNumTextViewArrayList.get(i)
+            var number = lottoNumArrayList.get(i)
+
+            numTxt.text = number.toString()
+        }
     }
 
     override fun setValues() {
 
+        thisWeekLottoNumTextViewArrayList.add(lottoNumTxt1)
+        thisWeekLottoNumTextViewArrayList.add(lottoNumTxt2)
+        thisWeekLottoNumTextViewArrayList.add(lottoNumTxt3)
+        thisWeekLottoNumTextViewArrayList.add(lottoNumTxt4)
+        thisWeekLottoNumTextViewArrayList.add(lottoNumTxt5)
+        thisWeekLottoNumTextViewArrayList.add(lottoNumTxt6)
+
+        myTextViewArrayList.add(myLottoNumTxt1)
+        myTextViewArrayList.add(myLottoNumTxt2)
+        myTextViewArrayList.add(myLottoNumTxt3)
+        myTextViewArrayList.add(myLottoNumTxt4)
+        myTextViewArrayList.add(myLottoNumTxt5)
+        myTextViewArrayList.add(myLottoNumTxt6)
+
+        for (myTv in myTextViewArrayList) {
+            myNumberArrayList.add(myTv.text.toString().toInt())
+        }
     }
 }
--- app/src/main/java/com/tjoeun/a201911_kotlinfinaltest/MainActivity.kt
+++ app/src/main/java/com/tjoeun/a201911_kotlinfinaltest/MainActivity.kt
@@ -3,16 +3,35 @@ package com.tjoeun.a201911_kotlinfinaltest
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
 
+    var lastBackButtonPressedTime = 0L
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setupEvents()
         setValues()
+    }
+
+    override fun onBackPressed() {
+//        super.onBackPressed()
+
+        val currentTime = System.currentTimeMillis() // 현재 시잔을 간볂하게 따고 싶을 때 사용
+
+        if (currentTime - lastBackButtonPressedTime > 1500) {
+//            백버튼을 오랜만에 누른 상황
+            Toast.makeText(mContext, "한번 더 뒤로 버튼을 누르면 앱이 종료 합니다", Toast.LENGTH_SHORT).show()
+
+        } else {
+            finish()
+        }
+
+        lastBackButtonPressedTime = currentTime
+
     }
 
     override fun setupEvents() {
